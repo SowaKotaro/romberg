5b5d459fb15d2f8f323aa28d4686e826cd70aebe
sowa <pty01m90@s.okayama-u.ac.jp>
2025-09-26 14:36:30 +0900

    success calculate

--- numerical_integration/job.sh
+++ numerical_integration/job.sh
@@ -10,7 +10,7 @@ module purge
 module load gcc/14.2.0
 module load openmpi/5.0.7-gcc
 
-mpicc -o trapezoidal.out trapezoidal_mpi.c
+mpicc -o trapezoidal.out trapezoidal_mpi.c -lm
 
 mpirun -npernode 192 -n 768 ./trapezoidal.out
 
